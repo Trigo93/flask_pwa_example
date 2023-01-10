98e516eadffd982141314d50687238b7a2509cf8
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2023-01-10 21:44:41 +0100

    add pwa example

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/sw.js')
+def sw():
+    return app.send_static_file('sw.js')
+
+if __name__ == '__main__':
+    app.run()
