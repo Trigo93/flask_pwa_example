9fd101afa5d763aef9537097e2dc5e27afba721d
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2023-01-11 16:12:52 +0100

    try to force header for server side in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
@@ -9,6 +9,12 @@ def index():
 @app.route('/sw.js')
 def sw():
     return app.send_static_file('sw.js')
+
+@app.route('/manifest.json')
+def manifest():
+    response = make_response(json.dumps(manifest))
+    response.headers['Content-Type'] = 'application/manifest+json'
+    return response
 
 if __name__ == '__main__':
     app.run()
