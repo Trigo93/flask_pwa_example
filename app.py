ef75444efc834003e5cc3fddb0cc6fde45b9c11f
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2023-01-11 16:23:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,12 +9,6 @@ def index():
 @app.route('/sw.js')
 def sw():
     return app.send_static_file('sw.js')
-
-@app.route('/manifest.json')
-def manifest():
-    response = make_response(json.dumps(manifest))
-    response.headers['Content-Type'] = 'application/manifest+json'
-    return response
 
 if __name__ == '__main__':
     app.run()
