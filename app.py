a3933a1b22b65ee910e329b2cdff217f19ceb9ff
Trigo93 <104326235+Trigo93@users.noreply.github.com>
2023-01-25 23:04:21 +0100

    Insure PWA best practice

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/sw.js')
+def serve_sw():
+    return send_file('sw.js', mimetype='application/javascript')
+
+@app.route('/manifest.json')
+def serve_manifest():
+    return send_file('manifest.json', mimetype='application/manifest+json')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
